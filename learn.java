890982857a0899a4138b56cd6497bfbdf47a75b6
Ngoc Anh <ngocanhnguyenhoang1235@gmail.com>
2025-04-26 20:49:52 +0700

    Da lam bai tap mot

---
+++ learn.java
@@ -0,0 +1,10 @@
+public class learn {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+
+        System.out.println("5 + 6 = " + (c));
+    }
+    
+}
